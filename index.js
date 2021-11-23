e0003aeb640e2676db238357ba43e829e3374e17
Tuan Nguyen <trentontuannguyen@gmail.com>
2021-11-22 22:49:45 -0500

    commit tree 1ab61962d4463310fe870c1fc5130e44a6a56d14

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(42 - someValue);
+}
+
+function distanceFromHqInFeet(someValue) {
+    let result = distanceFromHqInBlocks(someValue);
+    return result * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400) {
+        return 0;
+    } else if (distance >= 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+
+}
